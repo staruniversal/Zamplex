e5ed0b98af1da6e841c46638f3287049fb24a9af
Melody <80405720+staruniversal@users.noreply.github.com>
2021-08-13 01:45:22 +0900

    Add files via upload

--- src/zamplex/lib/screen/home_screen.dart
+++ src/zamplex/lib/screen/home_screen.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:netflix_clone_test/model/model_movie.dart';
 
 class Homescreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<Movie> movies =  [
+    Movie.fromMap({
+        'title': '사랑의 불시착',
+        'keyword': '사랑/로맨스/판타지',
+        'poster': 'test_movie_1.png'
+        'like': false
+      })
+    ];
   @override
   void initState() {
     super.initState();
@@ -12,10 +21,46 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('real home'),
-     ), 
-     );
+    return TopBar();
   }
+}
+
+class TobBar extends StatelessWidget {
+  @Override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(20, 7, 20, 7),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween, 
+        children: <Widget>[
+        Image.asset(
+          'images/zamplex_logo.png',
+          fit: BoxFit.contain, 
+          height: 25,
+        ),
+        Container(
+          padding: EdgeInsets.only(right: 1), 
+          child: Text(
+            'TV프로그램',
+            style: TextStyle(frontSize: 14),
+        ),
+        )
+      Container(
+          padding: EdgeInsets.only(right: 1), 
+          child: Text(
+            '영화',
+            style: TextStyle(frontSize: 14),
+        ),
+        ),
+      Container(
+          padding: EdgeInsets.only(right: 1), 
+          child: Text(
+            '내가 찜한 콘텐츠',
+            style: TextStyle(frontSize: 14),
+        ),
+        ),
+      ],
+    ), 
+  ); 
+ }
 }
---
+++ src/zamplex/lib/widget/carousel_slider.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:netflix clone test/model/model_movie.dart';
+
+class CarouselImage extends StatefulWidget {
+  final List<Movie> movies;
+  CarouselImage({this.movies});
+  _CarouselImageState createState() => _CarouselImageState();
+}
+
+class_CarouselImageState extends State<CarouselImage> {
+  @override
+  Void initState() {
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  
+  
+  }
+}
